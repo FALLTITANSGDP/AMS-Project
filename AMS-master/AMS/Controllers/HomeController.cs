ebb5d6f3cd0c7850f3633c80c2df99d05f750c3d
mahimabyrapuneni <mahima97.rc@gmail.com>
2022-10-24 14:20:26 -0500

    home page login

--- AMS-master/AMS/Controllers/HomeController.cs
+++ AMS-master/AMS/Controllers/HomeController.cs
@@ -207,7 +207,7 @@ namespace AMS.Controllers
                         AllowRefresh = true,
                         ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(25),
                         IssuedUtc = DateTime.UtcNow,
-                        RedirectUri = Url.Action("Index", "Home")
+                        RedirectUri = Url.Action("Index", "HomePage")
                     };
                     await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity), authProperties);
 
